337cbeeb0d6a9605b952a93a9812a2bf56d93c47
Muralikrishna Kacahpuram <>
2021-05-21 13:20:34 +0530

    insert at begining

---
+++ insert_all.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+struct node {
+	int data;
+	node* next;
+	node(int n){
+		data = n;
+		next = nullptr;
+	}
+};
+node* get_new_node(int n){
+	return new node(n);
+}
+node* insert_at_begin(node* head, int n){
+	node* new_node = get_new_node(n);
+	new_node->next = head;
+	return new_node;
+}
+
+void display(node* head){
+	while(head){
+		std::cout<<head->data<<" ";
+		head = head->next;
+	}
+	std::cout<<std::endl;
+}
+
+int main(){
+	node* head = nullptr;
+	head = insert_at_begin(head,1);
+	head = insert_at_begin(head,2);
+	head = insert_at_begin(head,3);
+	head = insert_at_begin(head,4);
+
+	//display
+	display(head);
+	return 0;
+}
